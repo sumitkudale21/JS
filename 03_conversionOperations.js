cd56dffcc318bcae9fe3043d5fb2bae5f5a70490
sumitkudale21 <sumitkudale3545@gmail.com>
2025-06-27 19:42:00 +0530

    Refactor conversion operations and add comparison examples

--- 03_conversionOperations.js
+++ 03_conversionOperations.js
@@ -25,6 +25,65 @@ let booleanIsLoggedIn = Boolean(isLoggedIn)
 let someNumber = 33
 
 let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+// *********************** Operations ********************************
+
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2/2);
+// console.log(2%2);
+
+let str1 = "hello"
+let str2 = " sumit"
+
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log(3 + 4 * 2 % 2);  // 3
+
+// console.log(((3 + 4) * 2) % 2); // o
+
+
+// console.log(+true); // 1
+// // console.log(true+); // ERROR
+// console.log(*""); // ERROE
+
+
+// let num1, num2, num3
+
+// num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+// gameCounter++;// 101
+++gameCounter;//101
+// console.log(gameCounter);
+
+let x = 3;
+const y = x++;
+
+console.log(`x:${x}, y:${y}`);
+// Expected output: "x:4, y:3"
+
+let a = 3;
+const b = ++a;
+
+console.log(`a:${a}, b:${b}`);
+// Expected output: "a:4, b:4"
+
+
 
