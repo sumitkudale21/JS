49fc5fc7ec26bfe6bf5f3abe3b1fcf83b74bd628
sumitkudale21 <sumitkudale3545@gmail.com>
2025-06-27 18:56:58 +0530

    Add conversion operations and type examples

---
+++ 03_conversionOperations.js
@@ -0,0 +1,30 @@
+let score = "true"
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+let valueInNumber = Number(score)
+
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber); // NaN => specialtype
+
+//  "33" => 33
+// "33abd" => NaN
+// true => 1; false => 0
+
+
+let isLoggedIn = ""
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+// 1=> true; 0=> false
+// ""=> false
+// "name"=> true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
