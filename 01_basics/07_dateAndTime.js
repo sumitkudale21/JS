162bb29045752e3bf54be1bd08cd53377cc912b1
sumitkudale21 <sumitkudale3545@gmail.com>
2025-06-28 10:35:42 +0530

    Add variable declarations, data type examples, and conversion operations in JavaScript

---
+++ 01_basics/07_dateAndTime.js
@@ -0,0 +1,31 @@
+// Dates
+
+// let myDate =  Date()
+// console.log(myDate);
+// console.log(typeof myDate);
+
+// let myDatewithnew = new Date()
+// console.log(myDatewithnew);
+// console.log(typeof myDatewithnew);
+
+let myDate = new Date()
+// console.log(myDate);
+console.log(myDate.getMonth());
+
+console.log(myDate.toLocaleString('default', {
+ era:"long"
+}));
+
+
+// console.log(myDate.toString());
+// console.log(myDate.toISOString());
+// console.log(myDate.toJSON());
+// console.log(myDate.toLocaleDateString());
+// console.log(myDate.toTimeString());
+// console.log(myDate.toUTCString());
+
+// console.log(new Date(2003, 6, 21).toDateString());
+
+let myTimeStamp = Date.now()
+// console.log(myTimeStamp);
+
